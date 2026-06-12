b9c56d1b1df4363142854e3bddfa739ad5f693e4
27kwhite <27kwhite@aos92.org>
2026-06-11 22:30:30 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,72 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (screen2 == 0) {
         scene.setBackgroundImage(assets.image`0-1`)
     }
+    if (screen2 == 1) {
+        scene.setBackgroundImage(assets.image`1`)
+    }
+    if (screen2 == 2) {
+        scene.setBackgroundImage(assets.image`2`)
+    }
+    if (screen2 == 3) {
+        scene.setBackgroundImage(assets.image`3`)
+    }
+    if (screen2 == 4) {
+        scene.setBackgroundImage(assets.image`4`)
+    }
+    if (screen2 == 5) {
+        scene.setBackgroundImage(assets.image`5`)
+    }
+    if (screen2 == 6) {
+        scene.setBackgroundImage(assets.image`6`)
+    }
+    if (screen2 == 7) {
+        scene.setBackgroundImage(assets.image`7`)
+    }
+    if (screen2 == 8) {
+        scene.setBackgroundImage(assets.image`8`)
+    }
+    if (screen2 == 9) {
+        scene.setBackgroundImage(assets.image`9`)
+    }
+    if (screen2 == 10) {
+        scene.setBackgroundImage(assets.image`10`)
+    }
+    if (screen2 == 11) {
+        scene.setBackgroundImage(assets.image`11`)
+    }
+    if (screen2 == 12) {
+        scene.setBackgroundImage(assets.image`12`)
+    }
+    if (screen2 == 13) {
+        scene.setBackgroundImage(assets.image`13`)
+    }
+    if (screen2 == 14) {
+        scene.setBackgroundImage(assets.image`14`)
+    }
+    if (screen2 == 15) {
+        scene.setBackgroundImage(assets.image`15`)
+    }
+    if (screen2 == 16) {
+        scene.setBackgroundImage(assets.image`16`)
+    }
+    if (screen2 == 17) {
+        scene.setBackgroundImage(assets.image`17`)
+    }
+    if (screen2 == 18) {
+        scene.setBackgroundImage(assets.image`18`)
+    }
+    if (screen2 == 19) {
+        scene.setBackgroundImage(assets.image`19`)
+    }
+    if (screen2 == 20) {
+        scene.setBackgroundImage(assets.image`20`)
+    }
+    if (screen2 == 21) {
+        scene.setBackgroundImage(assets.image`21`)
+    }
+    if (screen2 == 22) {
+        scene.setBackgroundImage(assets.image`22`)
+    }
     screen2 += 1
 })
 let screen2 = 0
